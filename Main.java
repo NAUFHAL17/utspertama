e1bc580f9113baf3605eac6fe03a86c19c9541ab
NAUFHAL17 <naufhalardyansyah5@gmail.com>
2023-05-11 17:48:24 +0700

    UTS PBO

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
+import pengguna.Mhs;
+
 class Main {
     public static void main(String[] args) {
+        
+        Mhs mhs = new Mhs("ini_username", "ini_password");
+
+        mhs.setNama("Naufhal Ardyansyah Wibowo");
+        mhs.setNim("G.111.20.0006");
+        mhs.setSmt(6);
+
+        System.out.println("Nama Mhs: " + mhs.getNama());
+        System.out.println("Nim Mhs: " + mhs.getNim());
+        System.out.println("Smt Mhs: " + mhs.getSmt());
+        System.out.println("Username: " + mhs.getUsername());
+        System.out.println("Password: " + mhs.getPassword());
+
 
     }
 }
